23ebd66e0cde1ddb5518b1fe0d31dc1ec6e7a048
Gabriel Whatley <whatley_gabriel@students.columbusstate.edu>
2024-04-24 10:33:32 -0400

    Added header

--- GUI-Development-21416-Gabriel-Whatley-FinalProject/Form1.vb
+++ GUI-Development-21416-Gabriel-Whatley-FinalProject/Form1.vb
@@ -1,3 +1,11 @@
-﻿Public Class Form1
+﻿﻿'Name: Gabriel Whatley - gabriel.whatley@gmail.com
+'Date: 4/24/24
+'Class: CPSC 3118 - Graphical User Interface Development - CRN:21416
+'Instructor: Bruce Montgomery
+'Program Description: Final assignment that converts measurements between inch and metric based on user input and saves them to a file.
 
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
