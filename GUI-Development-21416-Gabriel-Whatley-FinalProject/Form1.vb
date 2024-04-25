869da973ddb9720ec9883eb1c4101f567ed23ba7
Gabriel Whatley <whatley_gabriel@students.columbusstate.edu>
2024-04-25 01:50:37 -0400

    Program complete

--- GUI-Development-21416-Gabriel-Whatley-FinalProject/Form1.vb
+++ GUI-Development-21416-Gabriel-Whatley-FinalProject/Form1.vb
@@ -6,24 +6,33 @@
 
 Public Class Form1
 
+    Dim writer As System.IO.StreamWriter
     Dim input As Decimal
 
     Private Sub btn_save_list_Click(sender As Object, e As EventArgs) Handles btn_save_list.Click
-
+        writer = New IO.StreamWriter("measures.txt") 'Create a writer to write the file.
+        For Each item In listbox_results.Items 'iterate through the items in the listbox.
+            writer.WriteLine(item) 'Write each one to a new line in the file
+        Next
+        writer.Close() ' Close the writer when it's done.
+        lbl_save_msg.Text = String.Format("Saved {0} entries to messages.txt", listbox_results.Items.Count) 'Report the number of items that were written.
     End Sub
 
     Private Sub btn_clear_results_Click(sender As Object, e As EventArgs) Handles btn_clear_results.Click
+        'Clear the conversion output label and reset the selection on the inch to meter radio button.
         lbl_conversion_output.Text = ""
         rdio_In_to_M.Checked = True
     End Sub
 
     Private Sub btn_convert_Click(sender As Object, e As EventArgs) Handles btn_convert.Click
 
+        'Check that the input is a number, if not display an error message and exit the sub. If it can be parsed, store it in Decimal variable "input".
         If Decimal.TryParse(textbox_input.Text, input) = False Then
             MsgBox("Please enter a positive decimal number.", 0, "Warning")
             Exit Sub
         End If
 
+        'Check that the number is positive, if not display an error message and exit the sub.
         If input <= 0 Then
             MsgBox("Please enter a positive decimal number.", 0, "Warning")
             Exit Sub
@@ -42,13 +51,16 @@ Public Class Form1
     End Sub
 
     Private Sub btn_save_results_Click(sender As Object, e As EventArgs) Handles btn_save_results.Click
+        'If the output label isn't empty, then add the text from the output label to the listbox items.
         If lbl_conversion_output.Text IsNot "" Then
             listbox_results.Items.Add(lbl_conversion_output.Text)
         End If
     End Sub
 
     Private Sub btn_clear_list_Click(sender As Object, e As EventArgs) Handles btn_clear_list.Click
+        'Clear the listbox items and the save message label.
         listbox_results.Items.Clear()
+        lbl_save_msg.Text = ""
     End Sub
 
 End Class
